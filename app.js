b6a27cf4c957b0763eb6b87ecdc426ffe4f76494
patryk-pi <patryk.piasecki1@gmail.com>
2023-08-04 21:18:06 +0200

    added POST route for creating new tours

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const app = express();
-
 const port = 4000;
+
+app.use(express.json());
 
 // NEEDED to be done for __dirname to work!
 const __filename = fileURLToPath(import.meta.url);
@@ -35,7 +36,27 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.post('/api/v1/tours', (req, res) => {});
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
