f7ff1fc21830c6efa34df3719cd19f8f6d887a55
patryk-pi <patryk.piasecki1@gmail.com>
2023-08-04 21:02:12 +0200

    added post route for getting all the tours

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 
 const port = 4000;
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
+// NEEDED to be done for __dirname to work!
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
-});
+app.post('/api/v1/tours', (req, res) => {});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
