14e229e88e0a4061cd353812a3caa4437a02cb56
patryk-pi <patryk.piasecki1@gmail.com>
2023-08-06 22:36:56 +0200

    implemented env variables

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 import express from 'express';
-
+import dotenv from 'dotenv';
 import morgan from 'morgan';
+
+dotenv.config({ path: './config.env' });
 
 import tourRouter from './routes/tourRoutes.js';
 import userRouter from './routes/userRoutes.js';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// NEEDED to be done for __dirname to work!
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
-// MIDDLEWARES
-app.use(morgan('dev'));
+// MIDDLEWARES;
+
+console.log(process.env.NODE_ENV);
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
+
+app.use(express.static(`public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware 🙈');
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+import dotenv from 'dotenv';
 import app from './app.js';
 
-const port = 4000;
+dotenv.config({ path: './config.env' });
 
+// console.log(process.env);
+
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
