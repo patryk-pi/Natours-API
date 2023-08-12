4af05367cdd754f9e677f9f076a6737bec8c2f9d
patryk-pi <patryk.piasecki1@gmail.com>
2023-08-12 17:35:10 +0200

    setting up connection to ATLAS

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 import dotenv from 'dotenv';
 import app from './app.js';
+import mongoose from 'mongoose';
 
 dotenv.config({ path: './config.env' });
 
-// console.log(process.env);
+console.log(process.env);
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then((con) => {
+    console.log(con.connections);
+  });
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
