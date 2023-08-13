32cfe3c5655fd08a65f32a040a9ff52232288da7
patryk-pi <patryk.piasecki1@gmail.com>
2023-08-13 19:24:16 +0200

    implementing DELETE in API

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -135,7 +135,7 @@ export const updateTour = async (req, res) => {
         tour,
       },
     });
-  } catch {
+  } catch (err) {
     res.status(404).json({
       status: 'failed',
       message: err,
@@ -143,9 +143,17 @@ export const updateTour = async (req, res) => {
   }
 };
 
-export const deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+export const deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failed',
+      message: err,
+    });
+  }
 };
