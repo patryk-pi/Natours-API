b07490e0384dd00b35d5c59126074f5379677c4d
patryk-pi <patryk.piasecki1@gmail.com>
2023-08-15 13:22:13 +0200

    implemented basic filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -39,11 +39,21 @@ const __dirname = path.dirname(__filename);
 
 export const getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
+    const query = Tour.find(queryObj);
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
+      length: tours.length,
       data: {
         tours,
       },
