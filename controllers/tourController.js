e822835f0ebdac5a40b897bf55acee11c189f638
patryk-pi <patryk.piasecki1@gmail.com>
2023-08-16 21:00:49 +0200

    implemented advanced filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -40,13 +40,20 @@ const __dirname = path.dirname(__filename);
 export const getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
+
+    // 1. Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    // 2. Advanced filtering
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
 
     // EXECUTE QUERY
+    const query = Tour.find(JSON.parse(queryStr));
     const tours = await query;
 
     // SEND RESPONSE
