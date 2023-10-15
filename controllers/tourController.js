10ebc51e73ea1c0a3b0848931384228fdac408eb
patryk-pi <patryk.piasecki1@gmail.com>
2023-10-15 18:21:35 +0200

    limiting fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -52,17 +52,25 @@ export const getAllTours = async (req, res) => {
 
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
+    const query = Tour.find(JSON.parse(queryStr)); // Initialize the query
 
+    // 3. Sorting
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
+      query.sort(sortBy);
     } else {
-      query = query.sort('-createdAt');
+      query.sort('-createdAt');
+    }
+
+    // 4. Fields limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query.select(fields);
+    } else {
+      query.select('-__v');
     }
 
     // EXECUTE QUERY
-    const query = Tour.find(JSON.parse(queryStr));
     const tours = await query;
 
     // SEND RESPONSE
@@ -73,10 +81,6 @@ export const getAllTours = async (req, res) => {
       data: {
         tours,
       },
-      // result: tours.length,
-      // data: {
-      //   tours,
-      // },
     });
   } catch (err) {
     res.status(404).json({
