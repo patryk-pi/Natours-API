e6ddf71e5baaf0c0c9917a420963fe5a4bd678e1
patryk-pi <patryk.piasecki1@gmail.com>
2023-10-18 18:58:06 +0200

    aggregation pipeline: unwinding and projecting

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,7 @@ import {
   deleteTour,
   aliasTopTours,
   getTourStats,
+  getMonthlyPlan,
 } from './../controllers/tourController.js';
 
 const router = express.Router();
@@ -16,6 +17,7 @@ const router = express.Router();
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router.route('/').get(getAllTours).post(addNewTour);
 
