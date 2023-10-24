ae03df201e602b1003d3fc1197a37130cd3891fc
patryk-pi <patryk.piasecki1@gmail.com>
2023-10-24 13:26:05 +0200

    handled mongoose validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,10 +5,16 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
-handleDuplicateFieldsDB = (err) => {
+const handleDuplicateFieldsDB = (err) => {
   const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/);
   const message = `Duplicate field value: ${value}. Please use another value!`;
 
+  return new AppError(message, 400);
+};
+
+const handleValidationError = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
   return new AppError(message, 400);
 };
 
@@ -48,6 +54,7 @@ export const globalErrorHandler = (err, req, res, next) => {
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError') error = handleValidationError(error);
 
     sendErrorProd(error, res);
   }
